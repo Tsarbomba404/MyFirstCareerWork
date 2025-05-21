b8be55cf27208ae488e7d01c2bae03f033333880
Oluwatayo Olasunkanmi <sukkydboy@gmail.com>
2025-05-21 03:07:23 +0100

    update something

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import AllSection from "./section/AllSection.jsx";
+import { inject } from "@vercel/analytics";
 
+inject();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
+
     {/* <AllSection /> */}
   </StrictMode>
 );
